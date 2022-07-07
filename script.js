dd7e7755c0ea7d5a489ee558f4dcc4d714c4ae85
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2022-07-07 12:17:34 +0600

    Displaying map using Leaflet.

--- script.js
+++ script.js
@@ -16,6 +16,18 @@ if (navigator.geolocation)
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
+
+      var map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
